e085a7b223a474e8f2f209bbb88188a513293bf3
Fernando Andrade <fernandofreamunde@gmail.com>
2021-05-19 13:46:13 +0200

    remove coments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,11 @@
-// import { useEffect } from "react"; spa way
 
 export default function Home(props) {
-  // SSR and SSG
-  // console.log(props.episodes); //this comes from getServerProps where the api call is made by the nextJs server
-  
-  // normal react way to get data from api on page load
-  // SPA way of doing things
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/episodes')
-  //   .then(response => response.json())
-  //   .then(data => console.log(data))
-  // }, []);
 
   return (
     <div> Index </div>
   )
 }
 
-// For Server Side Render use getServerSideProps()
-// For Static Site Generation use getStaticProps() instead.
 export async function getStaticProps() {
   const response = await fetch('http://localhost:3333/episodes');
   const data = await response.json();
@@ -27,8 +14,6 @@ export async function getStaticProps() {
     props: {
       episodes: data,
     },
-    // for SSG pass revalidate to fetch new data each x amount of seconds
-    // in this case we go fetch podcasts every 8h:
     revalidate: 60 * 60 * 8,
   }
 }
