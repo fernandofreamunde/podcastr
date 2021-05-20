d53722c72e3fc702d583299c70fa52ec12bb9b99
Fernando Andrade <fernandofreamunde@gmail.com>
2021-05-20 13:41:05 +0200

    refactor PlayerContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,42 +2,24 @@ import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
-import { PlayerContext } from '../contexts/PlayerContext';
-import { useState } from 'react';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPLayingState(state: boolean) {
-    setIsPlaying(state);
-  }
 
 
   return (
     // the context must surround the components 
     // where its info will be used
     // in this case we went over kill and surrounded everything
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPLayingState}}>
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
-    </PlayerContext.Provider>
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
   )
 }
 
